49bed16f67f721b3fe0ebbb2800910876018fab1
Hantao Ye <1507431786@qq.com>
2021-08-28 11:07:37 +0800

    Update the 1st problem in Assignment 2

--- Assignment/Assignment-2/1799_Yeehaa.cpp
+++ Assignment/Assignment-2/1799_Yeehaa.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 #include <cstdio>
+#include <iomanip>
+#include <cmath>
+#define PI 3.1415926
 
 using namespace std;
 
+double radius(double R, int n)
+{
+    double t = sin(PI / n);
+    return t * R / (t + 1);
+}
+
+int main()
+{
+    int scene, n;
+    double R;
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> scene;
+    cout << fixed << setprecision(3);
+
+    for (int i = 1; i <= scene; i++)
+    {
+        cin >> R >> n;
+
+        cout << "Scenario #" << i << ":" << endl;
+        cout << radius(R, n) << endl;
+
+        if (i == scene)
+            break;
+
+        cout << endl;
+    }
+}
