69e3a290297e52ab413d9220ff6357d47582d3bc
Hantao Ye <1507431786@qq.com>
2021-08-31 13:44:58 +0800

    Update 9th problem in Assignment 1

--- Assignment/Assignment-1/1001_Exponentiation.cpp
+++ Assignment/Assignment-1/1001_Exponentiation.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <cstdio>
-#include <cstdlib>
-#include <string>
 
 using namespace std;
 
-string getPureNum(string base, int len);
-string removeEndZero(string input);
 string insertSeperator(string input, int sepIndex);
 string power(string base, int exponent);
 string stringMul(string x, string y);
@@ -16,73 +12,36 @@ int main()
     string base;
     int exponent;
 
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
 
     while (cin >> base >> exponent)
     {
-        int len = 5;
         int pointIndex = 0;
-        bool zeroEnd = true;
 
-        for (int i = base.size() - 1; i >= 0; i--)
-        {
-            if (zeroEnd && base[i] != '0')
-                zeroEnd = false;
-            else if (zeroEnd && base[i] == '0')
-                len--;
+        while (base[base.length() - 1] == '0')
+            base.erase(base.length() - 1);
 
-            if (base[i] == '.')
-            {
-                pointIndex = i;
-                break;
-            }
-        }
+        pointIndex = base.find('.');
+        base.erase(pointIndex, 1);
 
-        int sepIndex = (len - pointIndex) * exponent;
-        string input = getPureNum(base, len);
+        string output = power(base, exponent);
 
-        string output = removeEndZero(power(input, exponent));
+        int sepIndex = (base.length() - pointIndex) * exponent;
         cout << insertSeperator(output, sepIndex) << endl;
     }
 }
 
 string insertSeperator(string input, int sepIndex)
 {
-    if (sepIndex <= input.length())
+    if (sepIndex == 0)
+        return input;
+    else if (sepIndex <= input.length())
         input.insert(input.length() - sepIndex, ".");
     else
     {
         input.insert(0, "0");
         return insertSeperator(input, sepIndex);
     }
-
-    return input;
-}
-
-// get the input without the integer separator
-string getPureNum(string base, int len)
-{
-    string input = "";
-
-    for (int i = 0; i < len + 1; i++)
-    {
-        if (base[i] == '.')
-            continue;
-
-        input.append(to_string(base[i]-'0'));
-    }
-
-    return input;
-}
-
-// remove the end zero in the result
-string removeEndZero(string input)
-{
-    for (int i = input.length() - 1; i >= 0; i--)
-        if (input[i] == '0')
-            input.erase(input.end() - 1);
-        else
-            break;
 
     return input;
 }
@@ -108,7 +67,7 @@ string stringMul(string x, string y)
     if (!x.compare("0") || !y.compare("0"))
         return "0";
 
-    int res[x.length() + y.length() + 1] = {0};
+    string output(x.length() + y.length(), '0');
 
     for (int i = x.length() - 1; i >= 0; i--)
     {
@@ -116,23 +75,13 @@ string stringMul(string x, string y)
         for (int j = y.length() - 1; j >= 0; j--)
         {
             int num_y = y[j] - '0';
-            int sum = res[i + j + 1] + num_x * num_y;
-            res[i + j + 1] = sum % 10;
-            res[i + j] += sum / 10;
+            int sum = output[i + j + 1] - '0' + num_x * num_y;
+            output[i + j + 1] = sum % 10 + '0';
+            output[i + j] += sum / 10;
         }
     }
 
-    string output = "";
-    bool headZero = true;
-    for (int i = 0; i < x.length() + y.length() + 1; i++)
-    {
-        if (headZero && res[i] == 0)
-            continue;
-        else if (res[i] != 0)
-            headZero = false;
-
-        output.append(to_string(res[i]));
-    }
+    output.erase(0, output.find_first_not_of('0'));
 
     return output;
 }
